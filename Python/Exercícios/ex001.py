37dd66bb9ff3d2a75766052ae7f2f9e43f186182
Yuri Viegas <yuri.viegas9@gmail.com>
2023-03-29 08:57:39 -0300

    Melhorando Exercício

--- Python/Exercícios/ex001.py
+++ Python/Exercícios/ex001.py
@@ -1,16 +1,40 @@
 import os
+
 def clearTerminal():
     if os.name == 'nt':
         os.system("cls")
     else:
         os.system('clear')
+
+def showMenu():
+    print("O que desja fazer?\n")
+    print("1 - Alterar senha")
+    print("2 - Acesso ao sistema")
+    print("0 - Sair do menu\n")
+
 clearTerminal()
-password = int(input('Escolha uma senha: '))
-clearTerminal()
-verify = int(input('Insira a senha: '))
-while verify != password:
-    print('Senha Incorreta! Tente Novamente!')
-    os.system('pause')
+password = "1234"
+while True:
     clearTerminal()
-    verify = int(input('Insira a senha: '))
-print('Senha Correta!\nAcesso liberado!')
+    showMenu()
+    verify = (input("Escolha uma opção: "))
+    clearTerminal()
+    if verify == "1":
+        password = (input("Digite a nova senha: "))
+        print("Senha Alterada!")
+        os.system("pause")
+    elif verify == "2":
+        tryPass = (input("Insira a senha: "))
+        if tryPass == password:
+            print("Acesso permitido!")
+            os.system("pause")
+            break
+        else:
+            print("Acesso NEGADO!")
+            os.system("pause")
+            continue
+    elif verify == "0":
+        print("Saindo do Programa...")
+        os.system("pause")
+        break
+
